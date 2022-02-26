7b886855bde92ee8f8658ce4795047b1040101d5
Yong Hoon Shin <zcabyhs@ucl.ac.uk>
2022-02-26 18:29:59 +0000

    some conflicts fixed

--- dashboard/src/components/map.js
+++ dashboard/src/components/map.js
@@ -1,65 +1,65 @@
 import React, { useState, useRef, useEffect } from "react";
-// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-//mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-import ReactMapboxGl, { Layer, Source } from "react-mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-
-const M = ReactMapboxGl({
-  accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
-});
+// import ReactMapboxGl, { Layer, Source } from "react-mapbox-gl";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// const M = ReactMapboxGl({
+//   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+// });
 
 const stores = {
-  type: "geojson",
-  data: {
-    type: "Feature",
-    geometry: {
-      type: "Point",
-      coordinates: [-77.0323, 38.9131],
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-1.26012, 51.75577],
+      },
+      properties: {
+        title: "Mapbox DC",
+        "marker-symbol": "monument",
+      },
     },
-    properties: {
-      title: "Mapbox DC",
-      "marker-symbol": "monument",
-    },
-  },
+  ],
 };
 
 const Map = () => {
-  //   const mapContainer = useRef(null);
-  //   const map = useRef(null);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
   //   // coordinates for Oxford
-  //   const initialLng = -1.257677;
-  //   const initialLat = 51.752022;
   const initialZoom = [14];
-  const [x, y] = [-77.0323, 38.9131];
+  const [x, y] = [-1.26012, 51.75577];
 
-  //   useEffect(() => {
-  //     if (map.current) return; // initialize map only once
-  //     map.current = new mapboxgl.Map({
-  //       container: mapContainer.current,
-  //       style: "mapbox://styles/mapbox/streets-v11",
-  //       center: [initialLng, initialLat],
-  //       zoom: initialZoom,
-  //     });
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [x, y],
+      zoom: initialZoom[0],
+    });
 
-  //     map.current.on("load", () => {
-  //       /* Add the data to your map as a layer */
-  //       console.log("stores :>> ", stores);
-  //       map.current.addLayer({
-  //         id: "locations",
-  //         type: "circle",
-  //         source: {
-  //           type: "geojson",
-  //           data: stores,
-  //         },
-  //       });
-  //     });
-  //   });
+    map.current.on("load", () => {
+      /* Add the data to your map as a layer */
+      console.log("stores :>> ", stores);
+      map.current.addLayer({
+        id: "locations",
+        type: "circle",
+        source: {
+          type: "geojson",
+          data: stores,
+        },
+      });
+    });
+  });
 
   // <div ref={mapContainer} className="h-100" />
   return (
     <div className="h-100 flex-grow-1">
-      <M
+      <div ref={mapContainer} className="h-100" />
+      {/* <M
         style="mapbox://styles/mapbox/streets-v9"
         center={[x, y]}
         zoom={initialZoom}
@@ -71,7 +71,7 @@ const Map = () => {
         <Source id="source_id" geoJsonSource={stores} />
         <Layer type="circle" id="layer_id" sourceId="source_id" />
       </M>
-      ;
+      ; */}
     </div>
   );
 };
